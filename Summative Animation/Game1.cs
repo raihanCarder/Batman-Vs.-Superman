62b62a0785a2926f779a7a636db57b08a1396ad2
Raihan Carder <339509226@GoTVDSB.ca>
2023-11-24 11:03:43 -0500

    Done Background

--- Summative Animation/Game1.cs
+++ Summative Animation/Game1.cs
@@ -17,8 +17,12 @@ namespace Summative_Animation
         List<Texture2D> flyingSuperman = new List<Texture2D>();
 
         Screen screen;
-        bool fightBegins = true;
-
+        MouseState mouseState;
+        bool fightBegins = false;
+        int cityFrameCounter = 0;
+        float cityInterval = 0.06f;
+        float cityTime = 0;
+        float cityTimeStamp;
         enum Screen
         {
             Intro,
@@ -34,8 +38,8 @@ namespace Summative_Animation
             IsMouseVisible = true;
             _graphics.PreferredBackBufferWidth = 900;
             _graphics.PreferredBackBufferHeight = 500;
-            this.Window.Title = "Batman Vs. Superman";
             _graphics.ApplyChanges();
+            this.Window.Title = "Batman Vs. Superman";
         }
 
         protected override void Initialize()
@@ -44,10 +48,7 @@ namespace Summative_Animation
 
             screen = Screen.Intro;
 
-            for (int i = 0; i < 60; i++)    // Adds all city frames;
-            {
-                cityFrames.Add(Content.Load<Texture2D>($"frame_{i}_delay-0.04s"));
-            }
+            cityTimeStamp = 0;
 
             base.Initialize();
         }
@@ -56,25 +57,46 @@ namespace Summative_Animation
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            for (int i = 0; i < 60; i++)    // Adds all city frames;
+            {
+                cityFrames.Add(Content.Load<Texture2D>($"frame_{i}_delay-0.04s"));
+            }
+
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // add background code
 
+            cityTime = (float)gameTime.TotalGameTime.TotalSeconds - cityTimeStamp;
+
+            if (cityTime > cityInterval && fightBegins == true)
+            {
+                cityFrameCounter += 1;
+                cityTimeStamp = (float)gameTime.TotalGameTime.TotalSeconds;
+                if (cityFrameCounter >= 59)
+                {
+                    cityFrameCounter = 0;
+                }
+            }
+
 
 
             if (screen == Screen.Intro)
             {
-
+                fightBegins = false;
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.BeforeFight;
             }
             else if (screen == Screen.BeforeFight)
             {
-
+                fightBegins = true;
             }
             else if (screen == Screen.Fight)
             {
@@ -99,6 +121,15 @@ namespace Summative_Animation
             _spriteBatch.Begin();
 
             // Add Background Code
+            if (cityFrameCounter < 60)
+            {
+                _spriteBatch.Draw(cityFrames[cityFrameCounter], new Rectangle(0, 0, 900, 500), Color.White);
+            }
+            else
+            {
+                cityFrameCounter = 0;
+            }
+
 
 
             if (screen == Screen.Intro)
